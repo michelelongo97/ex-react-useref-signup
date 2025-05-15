5b4ef81ac4e531f01f0aff864fdc22d52b8dc43d
michelelongo97 <m.longo7x@gmail.com>
2025-05-15 18:38:32 +0200

    feat: add milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-import { use, useState } from "react";
+import { use, useMemo, useState } from "react";
+
+const letters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()-_=+[]{}|;:'\\,.<>?/`~";
 
 function App() {
   const [fullname, setFullname] = useState("");
@@ -7,6 +11,30 @@ function App() {
   const [specialization, setSpecialization] = useState("");
   const [years, setYears] = useState("");
   const [description, setDescription] = useState("");
+
+  const isUsernameValid = useMemo(() => {
+    const charsValid = username
+      .split("")
+      .every(
+        (char) =>
+          letters.includes(char.toLocaleLowerCase()) || numbers.includes(char)
+      );
+
+    return charsValid && username.length >= 6;
+  }, [username]);
+
+  const isPasswordValid = useMemo(() => {
+    return (
+      password.trim().length >= 8 &&
+      password.split("").some((char) => letters.includes(char)) &&
+      password.split("").some((char) => numbers.includes(char)) &&
+      password.split("").some((char) => symbols.includes(char))
+    );
+  }, [password]);
+
+  const isDescriptionValid = useMemo(() => {
+    return description.trim().length >= 100 && description.trim().length < 1000;
+  }, [description]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,7 +45,10 @@ function App() {
       !specialization.trim() ||
       !years.trim() ||
       years < 0 ||
-      !description.trim()
+      !description.trim() ||
+      !isUsernameValid ||
+      !isPasswordValid ||
+      !isDescriptionValid
     ) {
       alert("errore");
       return;
@@ -50,12 +81,26 @@ function App() {
           onChange={(e) => setUsername(e.target.value)}
           placeholder="Username"
         />
+        {username.trim() && (
+          <p style={{ color: isUsernameValid ? "green" : "red" }}>
+            {isUsernameValid
+              ? "Username valido"
+              : "Deve contenere almeno 6 caratteri alfanumerici"}
+          </p>
+        )}
         <input
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           placeholder="Password"
         />
+        {password.trim() && (
+          <p style={{ color: isPasswordValid ? "green" : "red" }}>
+            {isPasswordValid
+              ? "Password valida"
+              : "Deve contenere almeno 8 caratteri, 1 lettera, 1 numero ed 1 simbolo"}
+          </p>
+        )}
         <select
           value={specialization}
           onChange={(e) => setSpecialization(e.target.value)}
@@ -76,6 +121,13 @@ function App() {
           onChange={(e) => setDescription(e.target.value)}
           placeholder="Breve desrcizione dello sviluppaotore"
         />
+        {description.trim() && (
+          <p style={{ color: isDescriptionValid ? "green" : "red" }}>
+            {isDescriptionValid
+              ? "Descrizione valida"
+              : "Deve contenere almeno 100 caratteri e max 1000"}
+          </p>
+        )}
         <button type="submit">Invia</button>
       </form>
     </>
